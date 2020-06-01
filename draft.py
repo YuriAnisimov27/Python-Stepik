2c7266925d53f4e43cbe0d49888c61e5ae2c58a6
yurianisimov27 <yurianisimov27@gmail.com>
2020-06-01 12:19:47 +0300

    Added task 011-2

--- draft.py
+++ draft.py
@@ -1,13 +1,11 @@
 import xlrd
+
 f = open('answer.txt', 'w', encoding='utf-8')
 
 catalog = []
 for i in range(1, 1001):
-    file = xlrd.open_workbook(f'./rogaikopyta/{i}.xlsx')
-    sheet = file.sheet_by_index(0)
-    valname = sheet.row_values(1)[1]
-    valcash = int(sheet.row_values(1)[3])
-    catalog.append([valname, valcash])
+    file = xlrd.open_workbook(f'./rogaikopyta/{i}.xlsx').sheet_by_index(0)
+    catalog.append([file.row_values(1)[1], int(file.row_values(1)[3])])
 
 catalog.sort()
 for name in catalog:
