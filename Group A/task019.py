da3b8191b51c269bd8e821a44d7e9f0ad45f06a6
yurianisimov27 <yurianisimov27@gmail.com>
2020-06-09 16:36:35 +0300

    Added task 019-2

---
+++ Group A/task019.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+fout = open('index.html', 'w', encoding='utf8')
+
+print("Content-type: text/html")
+
+print("<html>", file=fout)
+print("<body>", file=fout)
+print("<table>", file=fout)
+for i in range(1, 11):
+    print("<tr>", file=fout)
+    for j in range(1, 11):
+        print("<td>", f'<a href="http://{i * j}.ru">{i * j}</a>', "</td>", file=fout)
+    print("</tr>", file=fout)
+print("</table>", file=fout)
+print("</body>", file=fout)
+print("</html>", file=fout)
+
+fout.close()
