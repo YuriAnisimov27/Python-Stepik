49cf489e8aa23a7c93d1f5af7eac727f4fe97166
yurianisimov27 <yurianisimov27@gmail.com>
2020-06-09 16:33:40 +0300

    Added task 019

--- cgi-bin/hello.py
+++ cgi-bin/hello.py
@@ -9,7 +9,7 @@ print("<table>", file=fout)
 for i in range(1, 11):
     print("<tr>", file=fout)
     for j in range(1, 11):
-        print("<td>", i * j, "</td>", file=fout)
+        print("<td>", f'<a href="http://{i * j}.ru">{i * j}</a>', "</td>", file=fout)
     print("</tr>", file=fout)
 print("</table>", file=fout)
 print("</body>", file=fout)
