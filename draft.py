20bcaeea23dd9caa79f059b03da904e0da5ebdf5
yurianisimov27 <yurianisimov27@gmail.com>
2020-06-19 18:40:06 +0300

    Added task 034

--- draft.py
+++ draft.py
@@ -1,11 +1,10 @@
-line = input()
-matrix = []
-while line != 'end':
-    matrix.append(list(map(int, line.split())))
-    line = input()
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        item = matrix[i - 1][j] + matrix[(i + 1) % len(matrix)][j] + matrix[i][j - 1] + matrix[i][
-            (j + 1) % len(matrix[i])]
-        print(item, end=' ')
-    print()
+def modify_list(l):
+    # for i in l:
+    #     if i % 2 != 0:
+    #         l.remove(i)
+    l = list(map(lambda x: x // 2, l))
+
+
+lst = [10, 5, 8, 3]
+modify_list(lst)
+print(lst)
