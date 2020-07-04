00c8cd88d95fbc6825d37548e1cd46bcf1527128
yurianisimov27 <yurianisimov27@gmail.com>
2020-07-04 21:57:57 +0300

    needed commit

---
+++ Group B/module3/task042.py
@@ -0,0 +1,5 @@
+from math import pi
+
+r = float(input())
+p = 2 * r * pi
+print(p)
--- draft.py
+++ draft.py
@@ -1,13 +1,3 @@
-text, code, text1, code1 = [input() for i in range(4)]
-text_dict, code_dict = dict(), dict()
-for i in range(len(text)):
-    if i not in text_dict:
-        text_dict[text[i]] = code[i]
-for i in range(len(code)):
-    if i not in code_dict:
-        code_dict[code[i]] = text[i]
-for i in text1:
-    print(text_dict[i], end='')
-print()
-for i in code1:
-    print(code_dict[i], end='')
+import sys
+
+print(*sys.argv[1:])
